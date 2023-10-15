dbeb0ccb36b297990888c7beb7ac0af011f1b2b6
Niwa <niwagameur@gmail.com>
2023-10-15 17:32:35 +0200

    Ajout du fichier Config.ini + Export path AutoHotKey

--- Ken_Kartana/script.py
+++ Ken_Kartana/script.py
@@ -1,8 +1,13 @@
 from ahk import AHK
-    
+import configparser
+
+# Chargez les variables depuis le fichier config.ini
+config = configparser.ConfigParser()
+config.read('config.ini')
+EXECUTABLE_PATH = config['DEFAULT']['EXECUTABLE_PATH']
+
 def hold_click(duration_ms):
-    #ahk = AHK(executable_path='C:\\Program Files\\AutoHotkey\\v1.1.37.01\\AutoHotkeyA32.exe')
-    ahk = AHK(executable_path='C:\Program Files\AutoHotkey\AutoHotkeyA32.exe')
+    ahk = AHK(executable_path=EXECUTABLE_PATH)
     
     with open("Script/drag.ahk", "r") as file:
         script = file.read().replace('%1%', str(duration_ms))
@@ -10,12 +15,9 @@ def hold_click(duration_ms):
     ahk.run_script(script, blocking=False)
 
 def simple_click():
-    #ahk = AHK(executable_path='C:\\Program Files\\AutoHotkey\\v1.1.37.01\\AutoHotkeyA32.exe')
-    ahk = AHK(executable_path='C:\Program Files\AutoHotkey\AutoHotkeyA32.exe')
+    ahk = AHK(executable_path=EXECUTABLE_PATH)
     
     with open("Script/simple_click.ahk", "r") as file:
         script = file.read()
     
     ahk.run_script(script, blocking=False)
-
-
